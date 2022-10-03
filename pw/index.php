d9ae1bfc9da9e01a8e883738db2601fc888f16ab
hapidzfadli <hapidzfadli@gmail.com>
2022-10-03 21:03:40 +0700

    Menambah navbar

--- pw/index.php
+++ pw/index.php
@@ -15,7 +15,13 @@ $buku = query("SELECT * FROM buku");
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
 </head>
 
-<body class="body-bg">
+<body>
+    <nav class="navbar navbar-light bg-light justify-content-between" style="padding: 20px;>
+        <a class=" navbar-brand">Toko Buku</a>
+        <a href="admin.php" class="my-2 my-sm-0">
+            <button type="button" class="btn btn-primary btn-sm">Admin</button>
+        </a>
+    </nav>
     <div class="d-flex flex-row" style="padding: 20px;">
         <div class="row">
             <?php foreach ($buku as $row) : ?>
