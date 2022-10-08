04922f852a68854bec42c18a05eb0891c46fbd6d
hapidzfadli <hapidzfadli@gmail.com>
2022-10-08 12:36:02 +0700

    Menyelesaikan Video 3 | Routing

--- mvc/app/core/App.php
+++ mvc/app/core/App.php
@@ -4,6 +4,14 @@ class App
 {
     public function __construct()
     {
-        echo 'OK';
+        $url = $this->parseURL();
+    }
+    public function parseURL()
+    {
+        if (isset($_GET['url'])) {
+            $url = rtrim($_GET['url'], '/');
+            $url = filter_var($url, FILTER_SANITIZE_URL);
+            $url = explode('/', $url);
+        }
     }
 }
